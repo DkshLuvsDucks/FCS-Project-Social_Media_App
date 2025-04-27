b14601ee453742f3eecac9aa2c5a45cc761ce99a
Daksh <dkshluvsducks@gmail.com>
2025-04-27 14:45:49 +0000

    deleted unecessary files

--- header-simple.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Getting headers from HTTPS endpoint..."
-curl -sIk https://192.168.2.241
--- security-header-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Checking headers directly with curl..."
-curl -I https://192.168.2.241/ -k
-
-echo -e "\nChecking headers for API endpoint..."
-curl -I https://192.168.2.241/api/health -k
-
